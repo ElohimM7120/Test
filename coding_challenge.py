79ecdaa6580bb71a52ab956ae3303a42627ffe89
ElohimM7120 <s1011598@student.knoxschools.org>
2026-02-19 17:04:48 +0000

    added the answer to the coding challenge.

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,4 +1,19 @@
 # 10 001st Prime
-By listing the first six prime numbers: 2,3,5,7,11, and 13 , we can see that the  prime is .
+def get_nth_prime(n):
+    primes = [2]
+    num = 3
+    while len(primes) < n:
+        is_prime = True
+        limit = num**0.5
+        for p in primes:
+            if p > limit:
+                break
+            if num % p == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        num += 2
+    return primes[-1]
 
-What is the  st prime number?
+print(get_nth_prime(10001))
